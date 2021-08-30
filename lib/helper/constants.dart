d9c9dc97fdb115984d9e61981ef21b01eebf6b6b
ErPrabin <prabinmaneeshrestha22@gmail.com>
2021-08-30 20:21:21 +0545

    asset folder added

---
+++ lib/helper/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kprimarycolor = Color(0xfffa4747);
+const ktextcolor = Color(0xff505050);
+const ktextlightcolor = Colors.grey;
+
+const ktextstyle = TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
+const ktextbanner = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+const kcategoryactivestyle = TextStyle(fontSize: 18, fontWeight: FontWeight.bold,letterSpacing: 1.8,color: kprimarycolor);
+const kcategorystyle = TextStyle(fontSize: 18, fontWeight: FontWeight.bold,letterSpacing: 1.8,color: ktextlightcolor);
--- lib/view/home/homepage.dart
+++ lib/view/home/homepage.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:smartcric/helper/constants.dart';
 
 class HomePage extends StatelessWidget {
   // const ({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('HomePage'),
-      
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        title: Text('K-Variety',
+            style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 24,
+                color: kprimarycolor)),
+        leading: IconButton(
+          iconSize: 18,
+          icon: Container(
+            height: 24,
+            width: 24,
+            child: SvgPicture.asset(
+              'assets/icons/menu.svg',
+              color: ktextlightcolor,
+            ),
+          ),
+          onPressed: () {},
+        ),
+      ),
     );
   }
 }
